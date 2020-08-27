286885183827c970ae50f623e0dce74b24726975
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2020-08-27 20:29:42 +0100

    Create blastn_MT-1.sh

---
+++ blastn_MT-1.sh
@@ -0,0 +1,19 @@
+#blastn: 2.10.1+
+# Package: blast 2.10.1, build May 12 2020 13:06:02
+ 
+files=$(ls *fasta)
+
+for FILE in  $(echo $files) ; do
+
+    head -n50  ${FILE} > temp_${FILE} 
+
+    echo BLASTn_results_${FILE}.txt 
+
+    blastn -db nt -query  temp_${FILE}  -out BLASTn_results_${FILE}.txt  -remote   -max_target_seqs 10
+
+
+    #rm temp_${FILE} 
+
+done
+
+
