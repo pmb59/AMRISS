6c068d5f12330167e862269f7cb22b0b24ac9c79
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2021-06-04 17:04:03 +0100

    Create DeepARG-LS.sh

---
+++ DeepARG/DeepARG-LS.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd  /GLIMMER/results/
+N=$( ls -a ORF*.fa | cut -d "." -f 1 )
+
+for j in $N ;
+do
+  cd /deeparg-ss
+  python deepARG.py --align --type nucl --genes --input /GLIMMER/results/${j}.fa --output /GLIMMER/deeparg/${j}.out
+done
+
