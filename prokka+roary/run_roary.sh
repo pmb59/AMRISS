3ecf5d3f73992af2b9872869d6a5fa4efe24f9bc
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2022-04-30 08:44:36 +0100

    Update run_roary.sh

--- prokka+roary/run_roary.sh
+++ prokka+roary/run_roary.sh
@@ -2,8 +2,7 @@
 
 docker pull sangerpathogens/roary
 
-# gff files are stored in $PWD/data
+# prokka gff files stored in $PWD/data
 
 docker run --rm -it -v $PWD/data:/data sangerpathogens/roary roary -r -p 4 -f /data/b_cereus ./data/*.gff
- 
 docker run --rm -it -v $PWD/data:/data sangerpathogens/roary roary -r -p 4 -f /data/e_bugandensis ./data/*.gff
