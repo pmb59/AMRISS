d992d257dbbc80386763a089e0609a44d56b787d
pmb59 <pmb59@users.noreply.github.com>
2022-09-21 00:34:03 +0000

    [auto] ci: apply beautysh changes

--- DeepARG/DeepARG-SS.sh
+++ DeepARG/DeepARG-SS.sh
@@ -6,8 +6,8 @@ L=$(ls F*.fastq.gz)
 
 cd /seqtk
 for i in $L; do
-  echo $i
-  seqtk seq -a /GLDS69/${i} >/GLDS69/${i}.fa
+    echo $i
+    seqtk seq -a /GLDS69/${i} >/GLDS69/${i}.fa
 done
 
 #merge R1 and R2 in one FASTA file
@@ -15,7 +15,7 @@ cd /GLDS69
 N=$(ls -a F*_R1-clean.fastq.gz | cut -d "R" -f 1)
 
 for j in $N; do
-  cat ${j}*.fa >${j}.fa
-  cd /deeparg-ss
-  python deepARG.py --align --type nucl --reads --input /GLDS69/${j}.fa --output /GLDS69/${j}.out
+    cat ${j}*.fa >${j}.fa
+    cd /deeparg-ss
+    python deepARG.py --align --type nucl --reads --input /GLDS69/${j}.fa --output /GLDS69/${j}.out
 done
--- prokka+roary/run_prokka.sh
+++ prokka+roary/run_prokka.sh
@@ -7,6 +7,6 @@ echo $contigs && popd
 docker pull staphb/prokka:latest
 
 for j in $contigs; do
-  docker run -v $PWD:/data staphb/prokka:latest prokka --prefix $j --cpus 2 --outdir /data/prokka-output/prokka-output-$j /data/MT-1/${j}.scaffolds.fasta
-  tar -zcvf ./prokka-output/prokka-output-${j}.tar.gz ./prokka-output/prokka-output-$j
+    docker run -v $PWD:/data staphb/prokka:latest prokka --prefix $j --cpus 2 --outdir /data/prokka-output/prokka-output-$j /data/MT-1/${j}.scaffolds.fasta
+    tar -zcvf ./prokka-output/prokka-output-${j}.tar.gz ./prokka-output/prokka-output-$j
 done
